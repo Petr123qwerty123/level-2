5b6e167f0d847287e680714b500483cde04bda84
kiryu <genazachto@gmail.com>
2023-11-02 19:47:50 +0300

    add comments and tests dev05

--- develop/dev05/task.go
+++ develop/dev05/task.go
@@ -28,6 +28,9 @@ import (
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
+var errNoArgs = errors.New("invalid number of arguments")
+
+// GrepFlags структура, определяющюая опции утилиты Grep
 type GrepFlags struct {
 	after      int
 	before     int
@@ -39,6 +42,7 @@ type GrepFlags struct {
 	lineNum    bool
 }
 
+// Parse метод для распарсивания и сохранения значений флагов опций в поля структуры GrepFlags
 func (gf *GrepFlags) Parse() {
 	flag.IntVar(&gf.after, "A", -1, "Output +N lines after match")
 	flag.IntVar(&gf.before, "B", -1, "Output +N lines before match")
@@ -52,18 +56,27 @@ func (gf *GrepFlags) Parse() {
 	flag.Parse()
 }
 
+// GrepArgs структура, определяющая неименованные аргументы запуска утилиты Grep
 type GrepArgs struct {
 	pattern    *regexp.Regexp
 	inputFiles []*os.File
 }
 
+// Parse метод для распарсивания и сохранения значений неименованных аргументов запуска утилиты Grep в поля структуры
+// GrepArgs
 func (ga *GrepArgs) Parse() error {
 	var err error
 
 	args := flag.Args()
 	nArg := flag.NArg()
 
+	// утилита имеет один обязательный неименованный аргумент - pattern, если агрументов 0 - выводим ошибку, если
+	// передан только один аргумент, воспринимаем его как pattern (регулярное выражение), в качестве источника поиска
+	// данных берем os.Stdin, в случае передачи более 1 аргумента воспринимаме первый как pattern, остальные - как
+	// пути до файлов (источников для поиска), открываем их и кладём *os.File в inputFiles
 	switch nArg {
+	case 0:
+		return errNoArgs
 	case 1:
 		pattern := flag.Arg(0)
 
@@ -73,7 +86,7 @@ func (ga *GrepArgs) Parse() error {
 		}
 
 		ga.inputFiles = append(ga.inputFiles, os.Stdin)
-	case 2:
+	default:
 		pattern := flag.Arg(0)
 		filenames := args[1:]
 
@@ -90,28 +103,31 @@ func (ga *GrepArgs) Parse() error {
 
 			ga.inputFiles = append(ga.inputFiles, inputFile)
 		}
-	default:
-		return errors.New("invalid number of arguments")
 	}
 
 	return nil
 }
 
+// GrepClient структура для управления утилитой Grep
 type GrepClient struct {
 	flags GrepFlags
 	args  GrepArgs
 	data  []string
 }
 
+// NewGrepClient конструктор для создания объекта структуры GrepClient
 func NewGrepClient() (*GrepClient, error) {
+	// создание пустого объекта структуры GrepClient
 	gc := &GrepClient{}
 
+	// парс флагов и аргументов запуска утилиты
 	gc.flags.Parse()
 	err := gc.args.Parse()
 	if err != nil {
 		return nil, err
 	}
 
+	// чтение и сохранение данных для поиска в поле data структуры GrepClient, закрытие reader'ов
 	for _, inputFile := range gc.args.inputFiles {
 		partData, err := utils.ReadData(inputFile)
 		if err != nil {
@@ -127,6 +143,17 @@ func NewGrepClient() (*GrepClient, error) {
 		gc.data = append(gc.data, partData...)
 	}
 
+	return gc, nil
+}
+
+// Grep метод для функционирования утилиты, который возвращает найденные по паттерну данные, исходя из установленных
+// опций при запуске утилиты
+func (gc *GrepClient) Grep() ([]string, error) {
+	var after, before int
+	var result []string
+	var err error
+
+	// учитывание опции -i, редактирование с учетом этой опции регулярного выражения
 	if gc.flags.ignoreCase {
 		gc.args.pattern, err = regexp.Compile("(?i)" + gc.args.pattern.String())
 		if err != nil {
@@ -134,6 +161,7 @@ func NewGrepClient() (*GrepClient, error) {
 		}
 	}
 
+	// учитывание опции -F, редактирование с учетом этой опции регулярного выражения
 	if gc.flags.fixed {
 		gc.args.pattern, err = regexp.Compile("^" + gc.args.pattern.String() + "$")
 		if err != nil {
@@ -141,16 +169,16 @@ func NewGrepClient() (*GrepClient, error) {
 		}
 	}
 
-	return gc, nil
-}
-
-func (gc *GrepClient) Grep() ([]string, error) {
-	var after, before int
-	var result []string
-
+	// длина слайса с данными для поиска
 	lenData := len(gc.data)
+	// создаем слайс []bool длиной, равной слайсу с данными для поиска, чтобы понимать по индексу слайса, какую строку
+	// из gc.data использовать, по умолчанию мы ничего не находим, то есть каждый элемент usingIndexes - false
 	usingIndexes := make([]bool, lenData)
 
+	// учитывание опций -A, -B, -C
+	// флаг -C в приоритете, если есть значение у -C, то after=before=gc.flags.context,
+	// если нет значение у флага -C, но есть у -A или -B, то after=gc.flags.after или before=gc.flags.before
+	// соответсвенно
 	if gc.flags.context > 0 {
 		after, before = gc.flags.context, gc.flags.context
 	} else {
@@ -163,12 +191,19 @@ func (gc *GrepClient) Grep() ([]string, error) {
 		}
 	}
 
+	// учитывание опции -v
+	// так как ниже мы ищем данные и меняем значения в usingIndexes на противоположные (true->false, false->true),
+	// то когда выставлен флаг -v мы сразу инвертируем каждый элемент usingIndexes (сразу все считается результатом),
+	// соответсвенно когда мы действительно что-то найдем по паттерну, мы изменим значение на противоположное, таким
+	// образом добьемся инвертации результата поиска (результат - все, кроме того, что найдено)
 	if gc.flags.invert {
 		for i := 0; i < lenData; i++ {
 			usingIndexes[i] = !usingIndexes[i]
 		}
 	}
 
+	// осуществление поиска нужных строк по паттерну с помощью итерации циклом по слайсу данных для поиска с учетом
+	// добавления строк до и после в соответствии флагам -A, -B, -C
 	for i, str := range gc.data {
 		if gc.args.pattern.MatchString(str) {
 			for j := i - before; j < i; j++ {
@@ -183,6 +218,10 @@ func (gc *GrepClient) Grep() ([]string, error) {
 				usingIndexes[i] = true
 			}
 
+			if usingIndexes[i] && gc.flags.invert {
+				usingIndexes[i] = false
+			}
+
 			for j := i + 1; j < i+after+1; j++ {
 				j := j
 
@@ -193,8 +232,13 @@ func (gc *GrepClient) Grep() ([]string, error) {
 		}
 	}
 
+	// с помощью слайса usingIndexes мы в result добавляем только те элементы gc.data, значение по индексу которых в
+	// слайсе usingIndexes - true
 	for index, use := range usingIndexes {
 		if use {
+			// учитывание опции -n
+			// если выставлен этот флаг, то к строке, добавляемой в result, прибавляется в начале её номер (индекс + 1)
+			// и ". " как сепаратор между номером и самой строкой
 			if gc.flags.lineNum {
 				result = append(result, strconv.Itoa(index+1)+". "+gc.data[index])
 			} else {
@@ -206,6 +250,7 @@ func (gc *GrepClient) Grep() ([]string, error) {
 	return result, nil
 }
 
+// Count возвращает количество найденных строк по паттерну или ошибку
 func (gc *GrepClient) Count() (int, error) {
 	searchResult, err := gc.Grep()
 	if err != nil {
@@ -218,6 +263,8 @@ func (gc *GrepClient) Count() (int, error) {
 }
 
 func (gc *GrepClient) Start() error {
+	// учитывание опции -c
+	// в вывод идут не сами найденные строки, а количество таких строк
 	if gc.flags.count {
 		outputData, err := gc.Count()
 		err = utils.WriteData(os.Stdout, outputData)
@@ -239,12 +286,14 @@ func main() {
 	var grepClient *GrepClient
 	var err error
 
+	// создание объекта структуры GrepClient, в случае ошибки - её вывод и выход из программы с кодом ошибки 1
 	grepClient, err = NewGrepClient()
 	if err != nil {
 		fmt.Printf("%q\n", err)
 		os.Exit(1)
 	}
 
+	// запуск утилиты, в случае - её вывод и выход из программы с кодом ошибки 1
 	err = grepClient.Start()
 	if err != nil {
 		fmt.Printf("%q\n", err)
--- develop/dev05/task_test.go
+++ develop/dev05/task_test.go
@@ -1,1 +1,881 @@
 package main
+
+import (
+	"flag"
+	"os"
+	"reflect"
+	"regexp"
+	"strings"
+	"testing"
+	"wb-level-2/develop/dev05/utils"
+)
+
+// Helper function to reset the command-line args.
+func resetArgs(args []string) {
+	os.Args = []string{"testArgs"}
+
+	for _, arg := range args {
+		os.Args = append(os.Args, arg)
+	}
+}
+
+func TestGrepFlags_Parse(t *testing.T) {
+	tests := []struct {
+		name  string
+		args  []string
+		flags GrepFlags
+	}{
+		{
+			name: "No flags",
+			args: []string{},
+			flags: GrepFlags{
+				after:   -1,
+				before:  -1,
+				context: -1,
+			},
+		},
+		{
+			name: "After flag",
+			args: []string{"-A", "5"},
+			flags: GrepFlags{
+				after:   5,
+				before:  -1,
+				context: -1,
+			},
+		},
+		{
+			name: "Before flag",
+			args: []string{"-B", "3"},
+			flags: GrepFlags{
+				after:   -1,
+				before:  3,
+				context: -1,
+			},
+		},
+		{
+			name: "Context flag",
+			args: []string{"-C", "2"},
+			flags: GrepFlags{
+				after:   -1,
+				before:  -1,
+				context: 2,
+			},
+		},
+		{
+			name: "Count flag",
+			args: []string{"-c"},
+			flags: GrepFlags{
+				after:   -1,
+				before:  -1,
+				context: -1,
+				count:   true,
+			},
+		},
+		{
+			name: "IgnoreCase flag",
+			args: []string{"-i"},
+			flags: GrepFlags{
+				after:      -1,
+				before:     -1,
+				context:    -1,
+				ignoreCase: true,
+			},
+		},
+		{
+			name: "Invert flag",
+			args: []string{"-v"},
+			flags: GrepFlags{
+				after:   -1,
+				before:  -1,
+				context: -1,
+				invert:  true,
+			},
+		},
+		{
+			name: "Fixed flag",
+			args: []string{"-F"},
+			flags: GrepFlags{
+				after:   -1,
+				before:  -1,
+				context: -1,
+				fixed:   true,
+			},
+		},
+		{
+			name: "LineNum flag",
+			args: []string{"-n"},
+			flags: GrepFlags{
+				after:   -1,
+				before:  -1,
+				context: -1,
+				lineNum: true,
+			},
+		},
+		{
+			name: "Invalid flag",
+			args: []string{"-x"},
+			flags: GrepFlags{
+				after:   -1,
+				before:  -1,
+				context: -1,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			flag.CommandLine = flag.NewFlagSet(tt.name, flag.ContinueOnError)
+			gf := GrepFlags{}
+
+			resetArgs(tt.args)
+			gf.Parse()
+
+			if gf != tt.flags {
+				t.Errorf("GrepFlags.Parse() got = %v, want %v", gf, tt.flags)
+			}
+		})
+	}
+}
+
+func TestGrepArgs_Parse(t *testing.T) {
+	tests := []struct {
+		name     string
+		args     []string
+		grepArgs GrepArgs
+		hasErr   bool
+	}{
+		{
+			name:     "No args",
+			args:     []string{},
+			grepArgs: GrepArgs{},
+			hasErr:   true,
+		},
+		{
+			name: "Pattern only",
+			args: []string{"test"},
+			grepArgs: GrepArgs{
+				inputFiles: []*os.File{os.Stdin},
+				pattern:    regexp.MustCompile("test"),
+			},
+			hasErr: false,
+		},
+		{
+			name:     "Invalid pattern",
+			args:     []string{"[invalid pattern"},
+			grepArgs: GrepArgs{},
+			hasErr:   true,
+		},
+		{
+			name: "Invalid filename",
+			args: []string{"test", "kjhg??fd"},
+			grepArgs: GrepArgs{
+				pattern: regexp.MustCompile("test"),
+			},
+			hasErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			flag.CommandLine = flag.NewFlagSet(tt.name, flag.ContinueOnError)
+			ga := GrepArgs{}
+
+			resetArgs(tt.args)
+
+			flag.Parse()
+			err := ga.Parse()
+
+			if (err != nil) != tt.hasErr {
+				t.Errorf("GrepArgs.Parse() error = %v, wantErr %v", err, tt.hasErr)
+				return
+			}
+
+			if !reflect.DeepEqual(ga.pattern, tt.grepArgs.pattern) {
+				t.Errorf("GrepArgs.Parse() pattern = %v, want %v", ga.pattern, tt.grepArgs.pattern)
+			}
+			if len(ga.inputFiles) != len(tt.grepArgs.inputFiles) {
+				t.Errorf("GrepArgs.Parse() inputFiles length = %v, want %v", len(ga.inputFiles), len(tt.grepArgs.inputFiles))
+			}
+		})
+	}
+}
+
+func TestGrepClient_Grep(t *testing.T) {
+	tests := []struct {
+		name           string
+		args           []string
+		data           []string
+		expectedResult []string
+	}{
+		{
+			name: "Search with no flags",
+			args: []string{"er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: []string{
+				"raspberry",
+				"watermelon",
+				"blueberry",
+			},
+		},
+		{
+			name: "Search with after flag",
+			args: []string{"-A", "3", "er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: []string{
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"blueberry",
+			},
+		},
+		{
+			name: "Search with before flag",
+			args: []string{"-B", "3", "er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: []string{
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+		},
+		{
+			name: "Search with context flag",
+			args: []string{"-C", "2", "er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: []string{
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+		},
+		{
+			name: "Search with after and context flag",
+			args: []string{"-A", "3", "-C", "2", "er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: []string{
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+		},
+		{
+			name: "Search with before and context flag",
+			args: []string{"-B", "3", "-C", "2", "er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: []string{
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+		},
+		{
+			name: "Search with after, before with context flag",
+			args: []string{"-A", "3", "-B", "4", "-C", "2", "er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: []string{
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+		},
+		{
+			name: "Search with ignore case flag",
+			args: []string{"-i", "er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: []string{
+				"strawbErry",
+				"raspberry",
+				"watermelon",
+				"cheRry",
+				"blueberry",
+			},
+		},
+		{
+			name: "Search with invert flag",
+			args: []string{"-v", "er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"grapes",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+			},
+		},
+		{
+			name: "Search with fixed flag",
+			args: []string{"-F", "er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: []string{},
+		},
+		{
+			name: "Search with line num flag",
+			args: []string{"-n", "er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: []string{
+				"7. raspberry",
+				"9. watermelon",
+				"15. blueberry",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			flag.CommandLine = flag.NewFlagSet(tt.name, flag.ContinueOnError)
+			resetArgs(tt.args)
+
+			gc := GrepClient{}
+
+			gc.flags.Parse()
+			err := gc.args.Parse()
+
+			gc.data = tt.data
+
+			result, err := gc.Grep()
+
+			if err != nil {
+				t.Errorf("not expected error: %q", err)
+			}
+
+			if len(result) == 0 && len(tt.expectedResult) == 0 {
+				return
+			}
+
+			if !reflect.DeepEqual(result, tt.expectedResult) {
+				t.Errorf("got %v, expected %v", result, tt.expectedResult)
+			}
+		})
+	}
+}
+
+func TestGrepClient_Count(t *testing.T) {
+	tests := []struct {
+		name           string
+		args           []string
+		data           []string
+		expectedResult int
+	}{
+		{
+			name: "Count searched data with no flags",
+			args: []string{"er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: 3,
+		},
+		{
+			name: "Count searched data with after flag",
+			args: []string{"-A", "3", "er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: 7,
+		},
+		{
+			name: "Count searched data with before flag",
+			args: []string{"-B", "3", "er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: 10,
+		},
+		{
+			name: "Count searched data with context flag",
+			args: []string{"-C", "2", "er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: 10,
+		},
+		{
+			name: "Count searched data with after and context flag",
+			args: []string{"-A", "3", "-C", "2", "er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: 10,
+		},
+		{
+			name: "Count searched data with before and context flag",
+			args: []string{"-B", "3", "-C", "2", "er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: 10,
+		},
+		{
+			name: "Count searched data with after, before with context flag",
+			args: []string{"-A", "3", "-B", "4", "-C", "2", "er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: 10,
+		},
+		{
+			name: "Count searched data with ignore case flag",
+			args: []string{"-i", "er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: 5,
+		},
+		{
+			name: "Count searched data with invert flag",
+			args: []string{"-v", "er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: 12,
+		},
+		{
+			name: "Count searched data with fixed flag",
+			args: []string{"-F", "er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: 0,
+		},
+		{
+			name: "Count searched data with line num flag",
+			args: []string{"-n", "er"},
+			data: []string{
+				"apple",
+				"kiwi",
+				"starfruit",
+				"strawbErry",
+				"pineapple",
+				"mango",
+				"raspberry",
+				"grapes",
+				"watermelon",
+				"grapefruit",
+				"banana",
+				"pear",
+				"cheRry",
+				"pomelo",
+				"blueberry",
+			},
+			expectedResult: 3,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			flag.CommandLine = flag.NewFlagSet(tt.name, flag.ContinueOnError)
+			resetArgs(tt.args)
+
+			gc := GrepClient{}
+
+			gc.flags.Parse()
+			err := gc.args.Parse()
+
+			gc.data = tt.data
+
+			result, err := gc.Count()
+
+			if err != nil {
+				t.Errorf("not expected error: %q", err)
+			}
+
+			if result != tt.expectedResult {
+				t.Errorf("got %v, expected %v", result, tt.expectedResult)
+			}
+		})
+	}
+}
+
+func TestReadData(t *testing.T) {
+	t.Run("Read data", func(t *testing.T) {
+		input := strings.NewReader("line1\nline2\nline3")
+		expected := []string{"line1", "line2", "line3"}
+
+		result, err := utils.ReadData(input)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("got %v, want %v", result, expected)
+		}
+	})
+}
+
+func TestWriteData(t *testing.T) {
+	t.Run("Write data", func(t *testing.T) {
+		var output strings.Builder
+		data := []string{"line1", "line2", "line3"}
+
+		err := utils.WriteData(&output, data...)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		expected := "line1\nline2\nline3\n"
+
+		if output.String() != expected {
+			t.Errorf("got %q, want %q", output.String(), expected)
+		}
+	})
+}
